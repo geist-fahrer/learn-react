00ed084663dffa2dd763cb3b78d75eb75c1a4600
Bishu Roy <biroy1393@gmail.com>
2025-08-25 10:24:37 +0000

    React js component with props

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png'
+import { CORE_CONCEPTS } from './data';
 
 const reactDescription = ["Fundamental", "Core", "Crucial"]
 function genRandomInt(max) {
@@ -17,12 +18,39 @@ function Header() {
     </header>
   );
 }
+
+function CoreConcepts({title, description, image}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              {...CORE_CONCEPTS[0]}
+            />
+            <CoreConcepts 
+              {...CORE_CONCEPTS[1]}
+            />
+            <CoreConcepts 
+              {...CORE_CONCEPTS[2]}
+            />
+            <CoreConcepts 
+              {...CORE_CONCEPTS[3]}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
