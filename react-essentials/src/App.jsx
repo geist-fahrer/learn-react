dd88d8436f1dc5542a148a0ceaad4efa74227704
Bishu Roy <biroy1393@gmail.com>
2025-08-26 13:12:23 +0000

    Loop for component rendering

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -32,27 +32,18 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts
-              {...CORE_CONCEPTS[0]}
-            />
-            <CoreConcepts
-              {...CORE_CONCEPTS[1]}
-            />
-            <CoreConcepts
-              {...CORE_CONCEPTS[2]}
-            />
-            <CoreConcepts
-              {...CORE_CONCEPTS[3]}
-            />
+            {
+              CORE_CONCEPTS.map(concept => <CoreConcepts key={concept.title} {...concept} />)
+            }
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton isSelected={selectedTab === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTab === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTab === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTab === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
           {tabContent}
         </section>
